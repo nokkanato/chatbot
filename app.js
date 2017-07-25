4066150a76a32829d4817e4be08ae0ceb8cd488f
NOK <nokkanato@gmail.com>
2017-07-25 09:02:16 +0700

    hiii

--- app.js
+++ app.js
@@ -22,9 +22,44 @@ app.get('/webhook/', function (req, res) {
     }
     res.send('No sir')
 })
+
+let token = "EAAHTBOfZCqEUBABcrb888Sax7kBJUVZA5ZBGZAt5Sy8fPdqZBvtZA5ygZBmUOIXZCcYCbZCdEZCzd1dZCKQEapZB3Tct6htjqHuw8nSXQVeIBXEWdllZBOJ7f6l1GHBZCXwcyBMGNXV0Q0OOaUVgAN1dTOY7bfnYtFFLrjfDFn8nJ9EZACJxZAbAVk8OTD9yn8GYm94E5I4ZD"
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
+function sendText(sender, text) {
+	let messageData = {text: text}
+	request({
+		url: "https://graph.facebook.com/v2.6/me/messages",
+		qs : {access_token: token},
+		method: "POST",
+		json: {
+			recipient: {id: sender},
+			message : messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log("sending error")
+		} else if (response.body.error) {
+			console.log("response body error")
+		}
+	})
+}
+app.post('/webhook/', function(req, res) {
+  let messaging_events = req.body.entry[0].messaging
+	for (let i = 0; i < messaging_events.length; i++) {
+		let event = messaging_events[i]
+		let sender = event.sender.id
+    if (event.message && event.message.text) {
+			let text = event.message.text
+			sendText(sender, "Text echo: " + text.substring(0, 100))
+		}
+	}
+	res.sendStatus(200)
+})
+
+
+
 
 
 //For avoidong Heroku $PORT error
